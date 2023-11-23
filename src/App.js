04ece5678dec541ae37eeca5c835c304f33b4801
muratozbk <slipstream11@yandex.com>
2023-11-23 19:58:07 +0300

    step counter

--- src/App.js
+++ src/App.js
@@ -6,21 +6,81 @@ const messages = [
   "Invest your new income 💲",
 ];
 
-function App() {
+export default function App() {
+  return (
+    <div>
+      <Steps />
+      {/* <Steps /> */}
+      <Counter />
+    </div>
+  )
+}
+
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const date = new Date('november 23 2023');
+  date.setDate(date.getDate() + count)
+
+  function handleUp() {
+    setStep(step + 1);
+  }
+  function handleDown() {
+    if (step > 1) {
+      setStep(step - 1);
+    }
+  }
+
+  function handleCDown() {
+    setCount(count - step);
+  }
+  function handleCUp() {
+    setCount(count + step);
+  }
+
+  return (
+    <div className="steps" >
+      <div className="top" style={{ display: 'flex' }}>
+        <button className="minus"
+          onClick={handleDown}>-</button>
+        <h3>Step: {step}  </h3>
+        <button className="plus"
+          onClick={handleUp}>+</button>
+      </div>
+
+      <div className="bottom" style={{ display: 'flex', marginTop: '1rem' }}>
+        <button className="plus"
+          onClick={handleCDown}>-</button>
+        <h3>Count: {count}  </h3>
+        <button onClick={handleCUp} className="plus">+</button>
+      </div>
+
+      <p>
+        <span>{count === 0 ? 'Today is ' : count > 0 ? `${count} days from today is ` : `${Math.abs(count)} days ago was `}</span>
+        <span>{date.toDateString()}</span> </p>
+    </div>
+  )
+}
+
+function Steps() {
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((curStep) => curStep - 1);
   }
 
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) {
+      setStep((s) => s + 1);
+      // setStep((s) => s + 1);
+    };
   };
 
   return (
-    <>
-      <button onClick={() => setIsOpen(!isOpen)} className="close">&times;</button>
+    <div>
+      <button onClick={() => setIsOpen((isOpn) => !isOpn)}
+        className="close">&times;</button>
 
       {isOpen &&
         <div className="steps" >
@@ -30,7 +90,8 @@ function App() {
             <div className={step === 3 ? 'active' : ''}>3</div>
           </div>
 
-          <p className="message">Step {step}: {messages[step - 1]} </p>
+          <p className="message">
+            Step {step}: {messages[step - 1]} </p>
 
           <div className="buttons">
             <button onClick={handlePrevious} style={{ backgroundColor: '#7950f2', color: '#fff' }}>Previous</button>
@@ -39,8 +100,6 @@ function App() {
 
           </div>
         </div>}
-    </>
+    </div>
   );
-}
-
-export default App;
+};
