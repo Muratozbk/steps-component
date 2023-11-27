a6bdd423a92af98432ffb1b2398cff650d9bdbf4
muratozbk <slipstream11@yandex.com>
2023-11-27 14:08:05 +0300

    delete item

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
@@ -7,13 +7,145 @@ const initialItems = [
 ];
 
 function App() {
+  const [items, setItems] = useState(initialItems);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item])
+  };
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter(item => item.id !== id))
+  };
+
   return (
-    <div className='app'>
-      {/* <Logo />
-      <Form />
-      <PackingList />
-      <Stats /> */}
-      <FlashCards />
+    <div className='app' style={{ textAlign: 'center' }}>
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem} />
+      <Stats />
+      {/* <FlashCards />
+      <Counter /> */}
+    </div>
+  )
+};
+
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>
+};
+
+function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // Date.now().toString().slice(-7);
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() }
+    console.log(newItem);
+
+    onAddItems(newItem);
+    setDescription('');
+    setQuantity(1);
+  };
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your trip?</h3>
+      <select value={quantity}
+        onChange={e => setQuantity(+e.target.value)}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map
+          ((num) => (
+            <option value={num} key={num}>
+              {num}
+            </option>
+          ))}
+      </select>
+      <input type="text"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        placeholder='Item...' />
+      <button>Add</button>
+    </form>
+  )
+};
+
+function PackingList({ items, onDeleteItem }) {
+
+  return (
+    <div className="list">
+      <ul >
+        {items.map((item) => (
+          <Item item={item}
+            onDeleteItem={onDeleteItem}
+            key={item.id} />
+        ))}
+      </ul>
+    </div>
+  )
+};
+
+function Item({ item, onDeleteItem }) {
+  return (
+    <li >
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.description} {item.quantity}
+      </span>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
+    </li>
+  )
+};
+
+function Stats() {
+  return (
+    <footer className='stats'>
+      <em>
+        💼 You have X items on your list, and you already packed X (X%)
+      </em>
+    </footer>
+  )
+};
+
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const date = new Date("november 26 2023");
+  date.setDate(date.getDate() + count);
+  function reset() {
+    setStep(1)
+    setCount(0)
+  };
+
+  return (
+    <div>
+      <div>
+        <input type="range" min={1} max={10}
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))} />
+        <span>Step: {step} </span>
+      </div>
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <input type="text" value={count} onChange={(e) => setCount(+e.target.value)} />
+        <button onClick={() => setCount(c => c + step)} >+</button>
+      </div>
+      <p>
+        <span>
+          {count === 0 ? "Today is " :
+            count > 0 ? `${count} days from today is ` :
+              `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()} </span>
+
+      </p>
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={reset}>Reset</button>
+        </div>
+      ) : null}
+
     </div>
   )
 };
@@ -67,82 +199,6 @@ function FlashCards() {
       </div>
     ))}
   </div>;
-}
-
-
-function Logo() {
-  return <h1>🌴 Far Away 💼</h1>
-};
-
-function Form() {
-  const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState(1);
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    // Date.now().toString().slice(-7);
-
-    if (!description) return;
-    const newItem = { description, quantity, packed: false, id: Date.now() }
-    console.log(newItem);
-
-    setDescription('');
-    setQuantity(1);
-  };
-
-  return (
-    <form className="add-form" onSubmit={handleSubmit}>
-      <h3>What do you need for your trip?</h3>
-      <select value={quantity}
-        onChange={e => setQuantity(+e.target.value)}>
-        {Array.from({ length: 20 }, (_, i) => i + 1).map
-          ((num) => (
-            <option value={num} key={num}>
-              {num}
-            </option>
-          ))}
-      </select>
-      <input type="text"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-        placeholder='Item...' />
-      <button>Add</button>
-    </form>
-  )
-};
-
-function PackingList() {
-
-  return (
-    <div className="list">
-      <ul >
-        {initialItems.map((item) => (
-          <Item item={item} key={item.id} />
-        ))}
-      </ul>
-    </div>
-  )
-};
-
-function Item({ item }) {
-  return (
-    <li >
-      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
-        {item.description} {item.quantity}
-      </span>
-      <button>❌</button>
-    </li>
-  )
-};
-
-function Stats() {
-  return (
-    <footer className='stats'>
-      <em>
-        💼 You have X items on your list, and you already packed X (X%)
-      </em>
-    </footer>
-  )
 }
 
 export default App
