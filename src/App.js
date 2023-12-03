80c05526d407d0a34a504d2fc229ee914ca7490d
muratozbk <slipstream11@yandex.com>
2023-12-03 11:27:11 +0300

    tip calculator

--- src/App.js
+++ src/App.js
@@ -41,9 +41,74 @@ function App() {
         onToggleItems={handleToggleItem}
         onClearList={handleClearList} />
       <Stats items={items} />
+      {/* <TipCalc /> */}
     </div>
   )
 };
+
+function TipCalc() {
+  const [bill, setBill] = useState("");
+  const [perctip1, setPercTip1] = useState(0);
+  const [perctip2, setPercTip2] = useState(0);
+  const tip = Math.floor(bill * ((perctip1 + perctip2) / 2 / 100));
+
+  function handleReset() {
+    setBill('');
+    setPercTip1(0)
+    setPercTip2(0)
+  }
+
+  return (
+    <div >
+      <p>Tip Calculator</p>
+      <label>How much was the bill?</label>
+      <input type="number"
+        value={bill}
+        onChange={e => setBill(+e.target.value)} />
+      <SelectPercentage percentage={perctip1} onSelect={setPercTip1}>
+        how did you like the service?
+      </SelectPercentage>
+      <SelectPercentage percentage={perctip2} onSelect={setPercTip2}>
+        how did your friend like the service?
+      </SelectPercentage>
+
+      {bill && (
+        <>
+          <p></p>
+          <Output bill={bill} tip={tip} />
+          <Reset onReset={handleReset} />
+        </>
+      )}
+    </div>
+
+  )
+};
+
+function SelectPercentage({ children, percentage, onSelect }) {
+  return (
+    <div>
+      <label>{children}</label>
+      <select value={percentage} onChange={(e) => onSelect(+e.target.value)}>
+        <option value="0">Dissatisfied (0%)</option>
+        <option value="5">It was okay (5%)</option>
+        <option value="10">Good (10%)</option>
+        <option value="20">amazing! (20%)</option>
+      </select>
+    </div>
+  )
+};
+function Output({ bill, tip }) {
+  return (
+    <h3>You pay ${tip + bill} (${bill} + ${tip} tip )</h3>
+  )
+};
+
+function Reset({ onReset }) {
+  return (
+    <button onClick={onReset}>Reset</button>
+  )
+
+}
 
 export default App
 
